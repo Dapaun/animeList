616dbc3af8e43fb58ca47b2fc350ad00f56724f8
dan.paun <danpaun@deloittece.com>
2022-03-30 16:27:48 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  React.useEffect(() => {
+    fetch('https://api.aniapi.com/v1/anime/11')
+      .then(response => response.json())
+      .then(json => console.log(json))
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Main page
     </div>
   );
 }
